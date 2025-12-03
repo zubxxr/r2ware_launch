1dfcc9562c271c6049a080b9664cc985b5013759
Zubair Islam <zubxxr@gmail.com>
2025-12-02 23:05:14 -0500

    Update r2ware_launch.py

--- launch/r2ware_launch.py
+++ launch/r2ware_launch.py
@@ -12,25 +12,25 @@ from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package='r2ware_launch',
-            executable='r2ware_launch_node',
-            output='screen',
-            prefix=[],
-        ),
+        # Node(
+        #     package='r2ware_launch',
+        #     executable='r2ware_launch_node',
+        #     output='screen',
+        #     prefix=[],
+        # ),
 
         # Include the first launch script
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource('/root/r2ware/src/sensing/r2ware_sensing/launch/r2ware_sensing_launch.py'),
+            PythonLaunchDescriptionSource('/home/jetson/r2ware/src/sensing/r2ware_sensing/launch/r2ware_sensing_launch.py'),
         ),
 
         # Include the second launch script
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource('/root/r2ware/src/control/r2ware_control/launch/r2ware_control_launch.py'),
+            PythonLaunchDescriptionSource('/home/jetson/r2ware/src/control/r2ware_control/launch/r2ware_control_launch.py'),
         ),
 
         # Include the third launch script
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource('/root/r2ware/src/status/r2ware_status/launch/r2ware_status_launch.py'),
+            PythonLaunchDescriptionSource('/home/jetson/r2ware/src/status/r2ware_status/launch/r2ware_status_launch.py'),
         ),
     ])
